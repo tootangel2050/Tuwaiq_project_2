488525345acb6ba4b39aebdb9db0e914606e9d41
Houryah <92294225+Houryah@users.noreply.github.com>
2021-11-22 15:23:42 +0300

    mainn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Main from './components/main/Main';
+
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div>
+		<Main />
+	</div>;
 };
 
 export default App;
